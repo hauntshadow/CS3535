afafb886a65cef8b1415f5910d91bf5fd7e35ae4
Chris S <smithc4@email.appstate.edu>
2015-02-18 13:27:26 -0500

    Update one_segment.py

--- one_segment/one_segment.py
+++ one_segment/one_segment.py
@@ -17,8 +17,9 @@ Author: Chris Smith, 02-05-2015
 Changes made to original one.py:
 
     - Changes made to take the first segment out of every beat.
-    - Does not take the first bar from every beat anymore.
+    - Does not take the first beat from every bar anymore.
 
+The original code is stored at this address: https://github.com/echonest/remix/blob/master/examples/one/one.py
 '''
 import echonest.remix.audio as audio
 
@@ -33,13 +34,19 @@ Example:
 def main(input_filename, output_filename):
     audiofile = audio.LocalAudioFile(input_filename)
     '''
+    This line got the bars of the song in the previous version:
     bars = audiofile.analysis.bars
+    
+    Now, this line gets the beats in the song:
     '''
     beats = audiofile.analysis.beats
     collect = audio.AudioQuantumList()
     '''
+    This loop got the first beat in each bar and appended them to a list:
     for bar in bars:
         collect.append(bar.children()[0])
+        
+    Now, this loop gets the first segment in each beat and appends them to the list:
     '''
     for b in beats:
         collect.append(b.children()[0])
